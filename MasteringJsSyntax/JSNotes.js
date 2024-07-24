c01d252e217fc287e8513377fcc61788a39f400c
Endy <endyaduba@outlook.com>
2024-07-24 07:33:17 -0400

    A few things on dates and some JS notes

---
+++ MasteringJsSyntax/JSNotes.js
@@ -0,0 +1,7 @@
+//primitive types are stored directly in the stack and examples of primitive types are String/Number/Boolean etc.
+
+//Reference Types are stored in the heap and accessed by reference. Examples are arrays/functions and objects.
+
+
+//some common shortcuts
+//ctrl + right arrow -> moves to the end of every word 
